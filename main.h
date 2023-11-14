5f8d6949088d1ba7c2ef833bbbd8b9cb6ddbc6e1
Kaybee973 <ewejeabdulkabir@gmail.com>
2023-11-14 14:59:48 -0800

    updated

--- main.h
+++ main.h
@@ -1,8 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <unistd.h>
-#include <string.h>
+#include <stdlib.h>
 #include <stdarg.h>
+
 int _printf(const char *format, ...);
+int binary (int num);
+int octal(int num);
 
 #endif
